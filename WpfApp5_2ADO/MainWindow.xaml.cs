0c422133fc44741d6dc7bf569965e2e1716139c1
Jibek14 <g.bulekbaeva74@mail.ru>
2020-06-25 10:58:06 +0600

    valid authorize

---
+++ WpfApp5_2ADO/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.Linq;
+using System.Text.RegularExpressions;
+
+namespace WpfApp5_2ADO
+
+{  
+    public partial class MainWindow : Window
+    {
+        string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=usersEmail;Integrated Security=True";
+       
+        public MainWindow()
+        {
+            InitializeComponent();  
+        }
+        private void CheckEmail()
+        {  DataContext db = new DataContext(connectionString);
+         const string EMAIL_PATTENR = @"(\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)";
+            Table<UserEmail> data = db.GetTable<UserEmail>();
+            if (Regex.IsMatch(userEmail.Text, EMAIL_PATTENR))
+            {
+                MessageBox.Show("Good!");
+                UserEmail userA = new UserEmail { Email = userEmail.Text, Name = userName.Text };
+                db.GetTable<UserEmail>().InsertOnSubmit(userA);
+                db.SubmitChanges();
+                foreach(var user in data)
+                {
+                    res.Text+="email: "+user.Email + " name: " + user.Name+"\r\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("not correct email,try again");
+            }
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            CheckEmail();
+        }
+    }
+}
---
+++ WpfApp5_2ADO/UserEmail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+namespace WpfApp5_2ADO
+{
+    [Table(Name ="UsersData")]
+   public class UserEmail
+    {
+        [Column (IsPrimaryKey =true,IsDbGenerated =true)]
+        public int Id { get; set; }
+        [Column (Name ="name")]
+        public string Name { get; set; }
+        [Column(Name ="email")]
+        public string Email { get; set; }
+    }
+}
